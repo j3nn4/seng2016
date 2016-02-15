677e3a17d2157c3b8d3d280db387576b287863e1
j3nn4 <jenna.ahvensalmi@gmail.com>
2016-02-15 13:52:17 +0200

    Versio 1 ja testitapaukset 99 -> 100 takaisin, for i in range(1, end) -> for i in range(1, end+1)

--- test.py
+++ test.py
@@ -11,6 +11,24 @@ class TestSuite(unittest.TestCase):
 
 
         self.failIf(app.calc(1) != 1)
+		
+	def test_three(self):
+		app = FizzBuzz()
+
+
+        self.failIf(app.calc(3) != "Fizz")
+		
+	def test_five(self):
+		app = FizzBuzz()
+
+
+        self.failIf(app.calc(5) != "Buzz")
+		
+	def test_fifteen(self):
+		app = FizzBuzz()
+
+
+        self.failIf(app.calc(15) != "FizzBuzz")
 
     def test_run(self):
         output = StringIO()
@@ -18,7 +36,7 @@ class TestSuite(unittest.TestCase):
         app = FizzBuzz()
         app.run(100, output)
 
-        self.failIf(len(output.getvalue().splitlines()) != 99)
+        self.failIf(len(output.getvalue().splitlines()) != 100)
 
 def main():
     unittest.main()
